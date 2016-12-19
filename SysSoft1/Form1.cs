0caf67b38ca1b32f4eac151577244fed49b3a3f6
Сиянчев Даниил Олегович <DSiyanchev@ibs.root.ibscorp.ru>
2016-12-19 14:08:24 +0400

    asd

--- SysSoft1/Form1.cs
+++ SysSoft1/Form1.cs
@@ -15,7 +15,7 @@ namespace SysSoft1
         private string InputData = "";
         private string InputQuery = "";
         private string OutputData = "";
-        private List<Tuple<string, int>>[] = new List<Tuple<string, int>>[]();
+        private List<List<Tuple<string, int>>> hashTable = new List<List<Tuple<string, int>>>();
         private OpenFileDialog openFileDialog1 = new OpenFileDialog();
         public Form1()
         {
@@ -58,9 +58,75 @@ namespace SysSoft1
             }
         }
 
+        private int calcHashFunc(int value)
+        {
+            return (value % 1097);
+        }
+
         private void DoItBtn_Click(object sender, EventArgs e)
         {
-            
+            System.IO.StreamReader fileReader = new System.IO.StreamReader(InputData);
+            List<string> tempList = new List<string>();
+            string line;
+            while((line = fileReader.ReadLine()) != null)
+            {
+                tempList.Add(line);
+            }
+
+            for (int i = 0; i < tempList.Count; i ++)
+            {
+                int tmpNum = 0;
+                for (int j = 0; j < tempList[i].Length; j++)
+                {
+                    tmpNum += Convert.ToInt16(tempList[i][j]);
+                }
+
+                tmpNum = calcHashFunc(tmpNum);
+                if (hashTable.Count == 0)
+                {
+                    List<Tuple<string, int>> tmp = new List<Tuple<string, int>>();
+                    Tuple<string, int> tmpTup = new Tuple<string, int>(tempList[i], tmpNum);
+                    tmp.Add(tmpTup);
+                    hashTable.Add(tmp);
+                }
+                else
+                {
+                    for (int k = 0; k < hashTable.Count; k++)
+                    {
+                        List<Tuple<string, int>> tmp = hashTable[k];
+                        if (tmpNum == tmp[0].Item2)
+                        {
+                            Tuple<string, int> tmpTup = new Tuple<string, int>(tempList[i], tmpNum);
+                            tmp.Add(tmpTup);
+                            hashTable[k] = tmp;
+                        }
+                        else
+                        {
+                            Tuple<string, int> tmpTup = new Tuple<string, int>(tempList[i], tmpNum);
+                            List<Tuple<string, int>> tmp2 = new List<Tuple<string, int>>();
+                            tmp2.Add(tmpTup);
+                            hashTable.Add(tmp2);
+                        }
+                    }
+                }
+            }
+
+            string testOut = "";
+
+            for (int i = 0; i < hashTable.Count; i ++)
+            {
+                List<Tuple<string, int>> tmp = hashTable[i];
+                for (int j = 0; j < tmp.Count; j ++)
+                {
+                    testOut += tmp[j].Item1 + " | " + tmp[j].Item2 + "\n";
+                }
+            }
+
+            MessageBox.Show(testOut);
+
+
+
+            fileReader = new System.IO.StreamReader(InputQuery);
         }
     }
 }
